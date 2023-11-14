7a8b65e37791db9fa88a94478556de568b06946c
Tomek <tomekp004@gmail.com>
2023-11-14 12:14:56 +0100

    Initial commit

---
+++ app/src/main/java/com/example/flashlight/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.flashlight
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.flashlight.ui.theme.FlashlightTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            FlashlightTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    FlashlightLayout()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun FlashlightLayout(modifier: Modifier = Modifier) {
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    FlashlightTheme {
+        FlashlightLayout()
+    }
+}
